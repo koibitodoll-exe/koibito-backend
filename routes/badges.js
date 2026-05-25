7d17764c30068a5b8a92cdc2c5162977c8830746
Liv <lvnroses@gmail.com>
2026-05-26 01:53:19 +0700

    Fix legacy auth imports

--- routes/badges.js
+++ routes/badges.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../db');
-const auth = require('../middleware/auth');
+const authMiddleware = require("../middleware/authMiddleware");
 
 router.get('/user/badges', auth, async (req, res) => {
   try {
